f29abaa2e52be3567c19507b5396014dee6bb412
Montsa <42146130+Montsa@users.noreply.github.com>
2019-07-24 21:55:04 -0600

    Final Homework Pypoll

--- PythonDataFolder/main.py
+++ PythonDataFolder/main.py
@@ -3,13 +3,13 @@ import os
 import csv
 
 # Lists to store data
-Dates = []
-PNL = []
-totalmonths = 0
-totalPNL = 0
+VoterID = []
+County = []
+Candidate = []
+totalVotes = 0
 
 # Set path for file
-csvpath = os.path.join("Resources", "budget_data.csv")
+csvpath = os.path.join("Resources", "election_data.csv")
 
 # Open the CSV
 with open(csvpath, newline="") as csvfile:
@@ -20,41 +20,35 @@ with open(csvpath, newline="") as csvfile:
 
     # start iteration to add to list for date, PNL, and total months
     for row in csvreader:
-        Dates.append(row[0])
-        PNL.append(row[1])
-        totalmonths += 1
+        VoterID.append(row[0])
+        County.append(row[1])
+        Candidate.append(row[2])
+        totalVotes += 1
+    Candidate.sort()
+    Votes = dict((c,Candidate.count(c),) for c in set(Candidate))
+    winner = max(Votes, key=Votes.get)
+    #Pair_candidate_VoterID = dict(zip(Candidate,VoterID))
 
-    # convert PNL from string to float
-    PNL = [float (p) for p in PNL]
-    # Create separate PNL list, remove first value. Will be used to 
-    # subtract from PNL to find month over month change
-    PNL2 = [x for p, x in enumerate(PNL) if p > 0]
-    # create list to capture month over month change
-    chg = [i - j for i, j in zip(PNL2, PNL)]
-    # zip files to allow printing of max/ min change values w/ dates
-    pybank = zip(Dates, PNL, chg)
-    pybank1 = zip(Dates, chg)
-    pybank2 = zip(Dates, chg)
-    # assign values
-    avgchgPNL = sum(chg)/len(PNL2)
-    totalPNL = sum(PNL)
-    GIncrease = max(chg)
-    GDecrease = min(chg)
-    DIncrease = [c for c,d in pybank1 if d == GIncrease]
-    DDecrease = [a for a,b in pybank2 if b == GDecrease]
-    #DDecrease = [d for d,c in pybank if c == GDecrease]
+print(
+f'''
+Election Results
+--------------------------
+Total Votes: {totalVotes} 
+--------------------------'''
+        )
+pctv=[]
+for i in Votes:
     
-    print(
-        f'''
-        Financial Analysis
-        ---------------------
-        Total Months: {totalmonths} 
-        Total: ${totalPNL} 
-        Average Change: ${avgchgPNL} 
-        Greatest Increase in Profits: {DIncrease[0]} (${GIncrease})
-        Greatest Decrease in Profits: {DDecrease[0]} (${GDecrease})
-         ''')
-
+    pct=f'{(round((Votes[i]/totalVotes),2)*100)}%'
+    pctv.append(pct)
+    print(i, pct,Votes[i])
+print(
+f'''
+--------------------------
+Winner: {winner}
+--------------------------'''
+)
+results = zip(Votes,pctv)
 # save the output file path
 output_file = os.path.join("output.csv")
 
@@ -62,6 +56,7 @@ output_file = os.path.join("output.csv")
 with open(output_file, "w", newline="") as datafile:
     writer = csv.writer(datafile)
 
-    writer.writerow(["Date", "Profit and Lose", "Month over Month Change"])
+    writer.writerow(["Candidate", "Percentage of Vote"])
 
-    writer.writerows(pybank)
+    writer.writerows(results)
+    
